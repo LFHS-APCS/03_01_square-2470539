de17810dc80d3e89cbb1aec7bb3fc99710f8cc81
2470539 <69973564+2470539@users.noreply.github.com>
2021-01-07 15:45:45 -0600

    Update Square.java

--- Square.java
+++ Square.java
@@ -1,1 +1,28 @@
-
+public class Square{
+  private double side;
+  public final int MAX_SIDE_LENGTH = 10;
+  public Square(double side){
+  }
+  public double getSide(){
+    return side;
+  }
+  public double setSide(double theSide){
+    if(theSide<=10 && theSide>0){
+    side = theSide;
+    return side;
+    } else{
+      return side;
+    }
+  }
+  public String toString(){
+    return "Square with side length = " + side;
+  }
+  public double area(){
+    double area = side*side;
+    return area;
+  }
+  public double diagonal(){
+    double diagonal = side*Math.sqrt(2);
+    return diagonal;
+  }
+}
